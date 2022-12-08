5c2eee11430b0189ca8707c3cc78185fa7dcb6ef
Nolatrae <nola.trae@gmail.com>
2022-12-08 15:05:37 +0800

    фикс цветов
    фикс цветов в интерполяции

--- interpolation/visual_interpolation.py
+++ interpolation/visual_interpolation.py
@@ -20,23 +20,25 @@ def get_xy(matrix_xy):
 def plot_interpolation(data, delt=1):
     plot.title("Линейная интерполяция")
     x, y = get_xy(data)
-    plot.scatter(x, y, color='blue')
+    plot.scatter(x, y, color='grey')
     intr_point = int.line_interpolation(data)
     extr_point = int.line_extrapolation(data, delt=delt)
-    plot.scatter(intr_point[0], intr_point[1], color="orange")
-    plot.scatter(extr_point[0], extr_point[1], color="orange")
+    plot.scatter(intr_point[0], intr_point[1], color="red")
+    plot.scatter(extr_point[0], extr_point[1], color="red")
     show_graph(x, y)
 
 
 def plot_peacemeal(data):
     plot.title("Кусочно-линейная интерполяция")
     x, y = get_xy(data)
-    plot.scatter(x, y, color="blue")
+    plot.scatter(x, y, color="grey")
     coord = int.piecemeal(data)
     in_coord = coord[1:-1]
     out_coord = [coord[0], coord[-1]]
-    plot.scatter(m.matrix_transposition(in_coord)[0], m.matrix_transposition(in_coord)[1], color="blue")
-    plot.scatter(m.matrix_transposition(out_coord)[0], m.matrix_transposition(out_coord)[1], color="blue")
+    plot.scatter(m.matrix_transposition(in_coord)[
+                 0], m.matrix_transposition(in_coord)[1], color="grey")
+    plot.scatter(m.matrix_transposition(out_coord)[
+                 0], m.matrix_transposition(out_coord)[1], color="grey")
     show_graph(x, y)
 
 
@@ -44,9 +46,10 @@ def plot_lagrange_polynom(data, delt=1):
     plot.title("Интерполяционный полином Лагранжа")
     arr_x = m.matrix_transposition(data)[0][:]
     x, y = get_xy(data)
-    plot.scatter(x, y, color="blue")
+    plot.scatter(x, y, color="grey")
     plot.axis([min(x) - 1, max(x) + delt, min(y) - delt, max(y) + delt])
-    x = [i / 10 for i in range((min(arr_x) - delt) * 10, (max(arr_x) + delt) * 10 + 1)]
+    x = [i / 10 for i in range((min(arr_x) - delt)
+                               * 10, (max(arr_x) + delt) * 10 + 1)]
     y = [int.lagrange_polinom(data, x[i]) for i in range(len(x))]
     show_graph(x, y)
 
